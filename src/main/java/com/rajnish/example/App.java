1d07830c6dd70a5cf4e68122f9883100fffd3871
rajnikantbaflipara <raj.c800@gmail.com>
2019-07-30 18:09:39 +0530

    Commit After Log

--- src/main/java/com/rajnish/example/App.java
+++ src/main/java/com/rajnish/example/App.java
@@ -1,5 +1,7 @@
 package com.rajnish.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App 
 {
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
 	@GetMapping("/greeting")
 	public String greetingMessage() {
+		LOGGER.info("Called Greeting Method....");
 		return "Waoo! Congrats Rajnikant Baflipara your application deployed successfully in Pivotal Cloud Foundry.";
 	}
 	
